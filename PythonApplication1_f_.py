cacc215755dc2e0d062bdd95fa7f4c54ef411a4e
Admin <Admin@GENACUD>
2023-03-24 23:30:06 +0200

    ex_1-done

--- PythonApplication1_f_.py
+++ PythonApplication1_f_.py
@@ -1,1 +1,11 @@
 
+x = int(input('number_1= '))
+y = int(input('number_2= '))
+z = int(input('number_3= '))
+value = int(input('\n1. sum\n2. product\n 1 or 2: '))
+if value == 1:
+	print(x+y+z)
+elif value == 2:
+	print(x*y*z)
+else:
+	print('1 or 2!')
