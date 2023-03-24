e319e025b316e68c21481349b5eb1d3a0f8f8127
Admin <Admin@GENACUD>
2023-03-25 00:17:31 +0200

    ex_3-done

--- PythonApplication1_f_.py
+++ PythonApplication1_f_.py
@@ -1,1 +1,9 @@
 
+x = int(input('metres: '))
+value = float(input('\n1. міля\n2. дюйм\n3. ярд\n 1 - 3: '))
+if value == 1:
+    print(x*0.000621371)
+elif value == 2:
+    print(x*39.3701)
+elif value == 3:
+    print(x*1.09361)
